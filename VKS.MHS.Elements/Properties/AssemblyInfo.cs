e81d11dcc0e8ae93e3f136fb35701f5227791160
Vladimir A. Kozhevnikov <vladimir.a.kozhevnikov@serve-u.net>
2017-04-20 22:11:16 +0300

    Tech Commit

---
+++ VKS.MHS.Elements/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("VKS.MHS.Elements")]
+[assembly: AssemblyDescription("Interface definitions for elements within house system.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VKS")]
+[assembly: AssemblyProduct("MHS")]
+[assembly: AssemblyCopyright("Vladimir A. Kozhevnikov")]
+[assembly: AssemblyTrademark("MHS")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
